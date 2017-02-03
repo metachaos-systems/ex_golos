b9ca9abe83a2fdf0c683c57fe796e67f5a6eeb09
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-02-03 14:12:57 +0200

    add get_content example result

--- lib/database_api.ex
+++ lib/database_api.ex
@@ -28,7 +28,37 @@ defmodule Golos.DatabaseApi do
   end
 
   @doc """
-  Get content by author and permlink
+  Get content by author and permlink.
+
+  Example response:
+    %{"max_accepted_payout" => "1000000.000 GBG",
+    "title" => "[объявление] Краудсейл и Шэрдроп. Дистрибьюция",
+    "category" => "ru--kraudseijl", "promoted" => "0.000 GBG",
+    "last_update" => "2016-12-06T15:36:54", "created" => "2016-12-05T16:43:03",
+    "parent_permlink" => "ru--kraudseijl", "total_vote_weight" => 0,
+    "json_metadata" => "{\"tags\":[\"ru--kraudseijl\",\"ru--shyerdrop\",\"ru--golos\"],\"users\":[\"golos\",\"crowdsale\",\"cyberdrop\",\"misha\",\"ether\",\"bender\",\"hipster\",\"litvintech\",\"vitaly-lvov\"],\"image\":[\"https://dl.dropboxusercontent.com/u/52209381/golos/golos.png\",\"https://dl.dropboxusercontent.com/u/52209381/golos/Screenshot%202016-12-05%2018.30.00.png\",\"https://dl.dropboxusercontent.com/u/52209381/golos/ico_final-min.jpg\",\"https://dl.dropboxusercontent.com/u/52209381/golos/Screenshot%202016-12-06%2002.25.05.png\",\"https://dl.dropboxusercontent.com/u/52209381/golos/card.png\"],\"links\":[\"https://docs.google.com/spreadsheets/d/1JwCAeRwsu4NzCG20UDM_CnEEsskl0wtvQ7VYjqi233A/edit?usp=sharing\",\"https://golos.io/@litvintech\"]}",
+    "last_payout" => "2017-01-15T11:00:06",
+    "total_payout_value" => "2412.784 GBG", "allow_replies" => true,
+    "children_rshares2" => "0", "id" => "2.8.30160",
+    "pending_payout_value" => "0.000 GBG", "children" => 15, "replies" => [],
+    "body" => "![wow](https://dl.dropboxusercontent.com/u/52209381/golos/golos.png)\n\n**Добрый день. В данный момент начинается дистрибьюция токенов Голоса**\n\n### 19.00 МСК - Шэрдроп. Начало дистрибьюции\n1. В соответствии с данной таблицей переведено 333 818 Голосов с аккаунта @golos на аккаунт @crowdsale, а также 300 Голосов в Силу Голоса для осуществления транзакций.\n**Баланс аккаунта: 27 405 818 Голосов**\n2. В соответствии с этой же таблицей переведено 97 356 Голосов с аккаунта @golos на аккаунт @cyberdrop, а также 200 Голосов в Силу Голоса для осуществления транзакций. Баланс аккаунта составил 10% от общего количества токенов. Так как после генезиса аккаунтам, которые попали в шэрдроп на генезис, было переведено 5 Голосов в Силу Голоса (в качестве регистрации), то с этого аккаунта обратно на @golos было переведено 41 720 Голосов в Голосах.\n**Баланс аккаунта: 4 535 096 Голосов**\n3. Переведено 197 134 Голосов в Силу Голоса @misha - последняя часть вознаграждения как сооснователя Голоса. \n\n\n![progress](https://dl.dropboxusercontent.com/u/52209381/golos/Screenshot%202016-12-05%2018.30.00.png)\n\n4. Запущено два скрипта для процессинга результатов краудсейла, которые подготовили две группы в команде разработчиков. Процесс займет около четырех-пяти часов. После результаты будут провалидированны всей командой основателей и опубликованы в этом посте.\n5. Запущен скрипт для распределения Силы Голоса по результатам шэрдропа. С аккаунта @cyberdrop в данный момент переводится 4 525 543 Голосов в Силу Голоса 4551 аккаунт. \n\n### 00.00 МСК - Шэрдроп. Конец дистрибьюции\n1. Закончена дистрибьюция шэрдроп Голосов в Силу Голоса 4551 аккаунтам. \nОстальные аккаунты, 3823, по расчетам, должны были получить меньше пяти Голосов - и им было начислено по 5 Голосов в Силу Голоса на старте сети. Перечислены 9551 Голос обратно на аккаунт @golos, в связи с перерасчетом в большую сторону при проведении дополнительной транзакции с @golos на @cyberdrop.\n\n2. Получено два снэпшота краудсейла. Сейчас команда обрабатывает данные, происходит кросс-валидация. В течении двух часов будет подготовлен массив транзакций по результатам краудсейла.\n\n* обновлено количество аккаунтов, а также токенов на правильные значения в соответствующие моменты времени\n\n* 6 декабря, около начало второго по МСК, **в блоке 1 411 200 включается Вестинг**\n\n* обнаружен аккаунт  @ether, который записал в свой json_metadata ico_address аккаунта @bender, соответственно @ether не участвовал в краудсейле\n\n* решена инфраструктурная проблема, которая приводила к перебоям в работе сайта\n\n### 03.00 МСК\n**[Результаты краудсейла. Таблица с дистрибьюцией. Список транзакций на ICO Голос](https://docs.google.com/spreadsheets/d/1JwCAeRwsu4NzCG20UDM_CnEEsskl0wtvQ7VYjqi233A/edit?usp=sharing)**\n\n### 03.15 МСК - Краудсейл. Начало дистрибьюции \n![ico final](https://dl.dropboxusercontent.com/u/52209381/golos/ico_final-min.jpg)\n**Запуск дистрибьюции @hipster @litvintech @vitaly-lvov**\n\n![start](https://dl.dropboxusercontent.com/u/52209381/golos/Screenshot%202016-12-06%2002.25.05.png)\n**Начало дистрибьюции Голосов в Силу Голоса инвесторам краудсейла.**\n\n### 04.30 МСК - Сделано 50% транзакций \n\n### 05.05 МСК - Сделано 75% транзакций\n\n### 05.15 МСК - Сделано 100% транзакций. Дистрибьюция закончена.\n* сделан снэпшот блокчейна\n\n**Да прибудет с вами Сила!**\n\n**Краудсейл окончен.**\n\nВнимание. Сейчас в личном кабинете вы видите расчет по краудсейлу и количество токенов, которые сейчас не актуальна. Актуальные данные о вашем количестве токенов вы можете увидеть (и проверить) в **[данной таблице](https://docs.google.com/spreadsheets/d/1JwCAeRwsu4NzCG20UDM_CnEEsskl0wtvQ7VYjqi233A/edit?usp=sharing)**.\n\n[![follow litvintech](https://dl.dropboxusercontent.com/u/52209381/golos/card.png)](https://golos.io/@litvintech)",
+    "active" => "2016-12-06T22:23:06", "net_rshares" => 0,
+    "author_rewards" => 10011558, "total_pending_payout_value" => "0.000 GBG",
+    "root_comment" => "2.8.30160", "max_cashout_time" => "1969-12-31T23:59:59",
+    "root_title" => "[объявление] Краудсейл и Шэрдроп. Дистрибьюция",
+    "allow_votes" => true, "percent_steem_dollars" => 10000,
+    "children_abs_rshares" => 0, "net_votes" => 90, "author" => "litvintech",
+    "curator_payout_value" => "112.100 GBG",
+    "permlink" => "obyavlenie-kraudseil-i-sherdrop-distribyuciya",
+    "url" => "/ru--kraudseijl/@litvintech/obyavlenie-kraudseil-i-sherdrop-distribyuciya",
+    "cashout_time" => "2017-02-14T11:00:06", "parent_author" => "",
+    "allow_curation_rewards" => true, "vote_rshares" => 0,
+    "reward_weight" => 10000,
+    "active_votes" => [%{"percent" => 1000, "reputation" => "15928643268388",
+       "rshares" => "1974529666496", "time" => "2016-12-05T17:02:39",
+       "voter" => "val", "weight" => "99631990926249375"}, %{...}, ...], "depth" => 0,
+    "mode" => "second_payout", "abs_rshares" => 0,
+    "author_reputation" => "22784203010137"}
   """
   @spec get_content(String.t, String.t) :: map
   def get_content(author, permlink) do
