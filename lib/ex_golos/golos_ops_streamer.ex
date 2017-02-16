3e81bb8d622ff79a2f9d17982830118447971fa6
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-02-16 22:27:58 +0200

    add operation unpacker

--- lib/ex_golos/golos_ops_streamer.ex
+++ lib/ex_golos/golos_ops_streamer.ex
@@ -2,26 +2,32 @@ defmodule Golos.Streamer do
   use GenServer
 
   @doc """
-  Starts the handle module
+  Starts the handler module
   """
-  def start_link do
-    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  def start_link(config) do
+    GenServer.start_link(__MODULE__, config, name: __MODULE__)
   end
 
-  def init(:ok) do
+  def init(config) do
     {:ok, %{"head_block_number" => last_height}} = Golos.get_dynamic_global_properties
     Process.send_after(self(), :tick, 3_000)
-    # Process.send_after(__MODULE__, :tick, 3_000)
     {:ok, %{last_height: last_height}}
   end
 
   def handle_info(:tick, state) do
     {:ok, data} = Golos.get_block(state.last_height)
-    IO.inspect data
+    IO.inspect unpack_operations(data)
     Process.send_after(self(), :tick, 3_000)
     state = put_in(state.last_height, state.last_height + 1)
     {:noreply, state}
   end
 
+  def unpack_operations(block) do
+     for tx <- block["transactions"] do
+      for op <- tx["operations"] do
+        op
+      end
+     end
+  end
 
 end
