ba471d2286ce5449adfe6104b517c028dbb09b03
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-02-16 22:39:03 +0200

    add comment ops struct

---
+++ lib/ex_golos/ops/comment.ex
@@ -0,0 +1,4 @@
+defmodule Golos.Ops.Comment do
+  @enforce_keys [:author, :body, :json_metadata, :parent_author, :parent_permlink, :permlink, :title ]
+  defstruct [:author, :body, :json_metadata, :parent_author, :parent_permlink, :permlink, :title ]
+end
