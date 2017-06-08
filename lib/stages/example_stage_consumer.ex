2dbffd4b4dda4ff8c04827763a6ded345b059118
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2017-06-08 15:07:38 +0300

    update example to match latest data types

--- lib/stages/example_stage_consumer.ex
+++ lib/stages/example_stage_consumer.ex
@@ -1,5 +1,6 @@
-defmodule Golos.Stage.Ops.ExampleConsumer do
+defmodule Golos.Stage.StructuredOps.ExampleConsumer do
   use GenStage
+  alias Golos.StructuredOps
   require Logger
 
   def start_link(args, options \\ []) do
@@ -7,17 +8,29 @@ defmodule Golos.Stage.Ops.ExampleConsumer do
   end
 
   def init(state) do
-    {:consumer, state, subscribe_to: state.subscribe_to}
+    Logger.info("Golos example consumer is initializing...")
+    {:consumer, state, subscribe_to: state[:subscribe_to]}
   end
 
   def handle_events(events, _from, state) do
     for op <- events do
-      Logger.info """
-      New operation:
-      #{inspect op}
-      """
+      process_event(op)
     end
     {:noreply, [], state}
+  end
+
+  def process_event({%StructuredOps.Reblog{} = op_data, %{height: h, timestamp: t} =  op_metadata}) do
+      Logger.info """
+      Новый реблог:
+      #{inspect op_data} в блоке #{h}, время: #{t}
+      """
+  end
+
+  def process_event({op_data, %{height: h, timestamp: t} =  op_metadata}) do
+      Logger.info """
+      Новая операция:
+      #{inspect op_data} в блоке #{h}, время: #{t}
+      """
   end
 
 end
