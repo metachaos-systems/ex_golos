2baa2c58efe1f5576e2d3fbc6bf297f1e32478fe
ontofractal <ontofractal@protonmail.com>
2018-05-07 12:13:05 +0100

    remove fns used in another api

--- lib/social_network_api.ex
+++ lib/social_network_api.ex
@@ -382,31 +382,4 @@ defmodule Golos.SocialNetworkApi do
     call("get_active_votes", [account, permlink])
   end
 
-  @doc """
-  Get followers. Accepts account, starting follower, follow type (blog, ignore), limit of results.
-  Returns followers in ascending alphabetical order.
-
-  Example response:
-  ```
-  %{"follower" => "aim", "following" => "academy",
-            "id" => "8.0.21098", "what" => ["blog"]},
-  %{"follower" => "aleco", "following" => "academy",
-            "id" => "8.0.20183", "what" => ["blog"]},
-     %{...}, ...] ```
-  """
-  @spec get_followers(String.t(), String.t(), String.t(), integer) :: [map]
-  def get_followers(account, start_follower, follow_type, limit) do
-    Golos.call(["follow_api", "get_followers", [account, start_follower, follow_type, limit]])
-  end
-
-  @doc """
-  Get followings. Accepts account, starting following, follow type (blog, ignore), limit of results.
-  Returns followings in ascending alphabetical order.
-
-  Example response is the same as in get_followers.
-  """
-  @spec get_following(String.t(), String.t(), String.t(), integer) :: [map]
-  def get_following(account, start_follower, follow_type, limit) do
-    Golos.call(["follow_api", "get_following", [account, start_follower, follow_type, limit]])
-  end
 end
