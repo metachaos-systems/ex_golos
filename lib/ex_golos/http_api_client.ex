051f02fc116f3db2c17ddbc4ab04ed5bd72c28ff
ontofractal <ontofractal@protonmail.com>
2018-05-09 15:42:09 +0100

    add http transport

---
+++ lib/ex_golos/http_api_client.ex
@@ -0,0 +1,10 @@
+defmodule Steemex.HttpClient do
+  alias JSONRPC2.Clients.HTTP
+
+  @url Application.get_env(:ex_golos, :api_url)
+
+  def call([api, method, args]) do
+    response = HTTP.call(@url, "#{api}.#{method}", args)
+    {:ok, response}
+  end
+end
