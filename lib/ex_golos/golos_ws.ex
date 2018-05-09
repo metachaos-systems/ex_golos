1f5b2d1572bf5fd20cfa070cb0d7b124961e1c44
ontofractal <ontofractal@protonmail.com>
2018-05-09 15:59:22 +0100

    naming fix

--- lib/ex_golos/golos_ws.ex
+++ lib/ex_golos/golos_ws.ex
@@ -1,37 +1,70 @@
 defmodule Golos.WS do
-  use WebSockex
+  alias Poison, as: JSON
   require Logger
+  @moduledoc """
+  Starts the WebSocket server for given ws URL. Received messages
+  are forwarded to the sender pid
+  """
 
-  def start_link(url, opts \\ []) do
-    opts = Keyword.merge(opts, [name: :golos_ws])
-    WebSockex.start_link(url, __MODULE__, %{}, opts)
+  def start_link(url) do
+    :crypto.start
+    :ssl.start
+    golos_wss_url = String.to_charlist(url)
+
+    {:ok, sock_pid} = :websocket_client.start_link(golos_wss_url, __MODULE__, [])
+    # websocket_client doesn't pass options to the gen_server, so registering manually
+    Process.register(sock_pid, Golos.WS.Alt)
+    {:ok, sock_pid}
   end
 
 
-  def handle_connect(_conn, state) do
-    Logger.info("Golos websocket is connected!")
+  def init([], _conn_state \\ []) do
+    {:once, %{}}
+  end
+
+  def onconnect(_ws_req, state) do
     {:ok, state}
   end
 
-  def handle_info({:ssl_closed, _}, state) do
-    exit("Golos WS ssl connection closed")
+  def ondisconnect({:remote, :closed}, state) do
+    {:reconnect, state}
   end
 
-  def handle_frame({:text, msg}, state) do
-    data = Poison.decode!(msg)
+  @doc """
+  Closes the socket
+  """
+  def close(socket) do
+    send(socket, :close)
+  end
+
+  @doc """
+  Receives JSON encoded message from remote WS endpoint and
+  forwards message to client sender process
+  """
+  def websocket_handle({:text, msg}, _conn_state, state) do
+    data = JSON.decode!(msg)
     id = data["id"]
-    {from, params} = Golos.IdStore.get(id)
-    send(from, {:ws_response, {id, params, data}})
+    {pid, params} = Golos.IdStore.get(id)
+    send(pid, {:ws_response, {id, params, data}})
+
     {:ok, state}
   end
 
-  def handle_disconnect(%{reason: {:local, reason}}, state) do
-    Logger.info("Local close with reason: #{inspect reason}")
-    {:ok, state}
+  @doc """
+  Sends JSON encoded message to remote WS endpoint
+  """
+  def websocket_info({:send, msg}, _conn_state, state) do
+    {:reply, {:text, json!(msg)}, state}
   end
 
-  def handle_disconnect(disconnect_map, state) do
-    super(disconnect_map, state)
+  def websocket_info(:close, _conn_state, _state) do
+    {:close, <<>>, "done"}
   end
 
+  def websocket_terminate(_reason, _conn_state, _state) do
+    :ok
+  end
+
+  defp json!(map), do: JSON.encode!(map)
 end
+1
