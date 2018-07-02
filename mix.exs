0d495f33bbf869cb365fb328c0e8ebd2b90f7bbb
ontofractal <ontofractal@protonmail.com>
2018-07-02 18:28:16 +0100

    minor version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Golos.Mixfile do
   def project do
     [
       app: :ex_golos,
-      version: "0.11.0",
+      version: "0.11.1",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
